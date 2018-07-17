1ce74c67af66984d5d3b8a0b1a2d5a849f1ffd2a
allison.kushima <allisonkushima@gmail.com>
2018-07-16 23:54:00 -0300

    complete useful version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Reactive Spring Web Flux + Angular';
+  description = 'Angular + Spring Web Flux';
+
+  constructor() { }
+
 }
